fe7f24ea46e83178680e736bb8465dba853e2d3c
simplify-web <simplify-web@byom.de>
2019-08-26 11:53:28 +0200

    import exapmle project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Netlify CMS + VuePress',
+  description: 'Netlify + VuePress',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'simplify-web/www',
+    sidebar: [
+      '/',
+      '/welcome',
+      '/test'
+    ],
+    nav: [
+      {
+        text: 'Admin',
+        link: '/admin/#/',
+      }
+    ]
+  }
+}
---
+++ docs/test.md
@@ -0,0 +1,4 @@
+---
+title: My First Netlify Post
+---
+first post text
