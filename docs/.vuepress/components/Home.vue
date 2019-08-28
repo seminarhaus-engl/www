6c571a7128e9dacfbf7e2716a20da718632bec07
simplify-web <simplify-web@byom.de>
2019-08-28 15:01:46 +0200

    add custom layout for home page

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,22 @@
+<!-- .vuepress/components/CustomLayout.vue -->
+
+<template>
+  <div class="home-layout">
+
+    <!-- .main-div is the block with white background -->
+    <div class="main-div">
+      This is a custom layout. it works
+    </div>
+
+    <div class="main-div">
+      <!-- <Content /> is to show the rendered markdown content of this page -->
+      <Content class="content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeLayout',
+}
+</script>
