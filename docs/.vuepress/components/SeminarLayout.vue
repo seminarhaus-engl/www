9c5e7d3bb20df154dcd9e95b603b486b9a6525ef
akralj <dev@kralj.de>
2019-11-13 12:29:36 +0100

    use forked meteorlxy theme with pimped infobox

--- docs/.vuepress/components/SeminarLayout.vue
+++ docs/.vuepress/components/SeminarLayout.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
 
   .page
-    h2 {{fm.title}}
-    p.seminar-info-list {{fm.startDate}} - {{fm.endDate}} 
-    p(v-if="descriptionShort",  v-html='descriptionShort')
-    p(v-if="description",       v-html='description')
-    p(v-if="fm.honorar")      <strong>Honorar:</strong>                     {{fm.honorar}}
-    p(v-if="fm.kursgebuehr")  <strong>Kursbebühr:</strong>                  {{fm.kursgebuehr}}
-    p(v-if="fm.unterkunft")   <strong>Unterkunft und Verpflegung:</strong>  {{fm.unterkunft}}
-    p(v-if="fm.performers")   <strong>Leitung:</strong>                     {{fm.performers}}
-    p(v-if="dozentenbeschreibung", v-html="dozentenbeschreibung")
-    a(v-if="fm.websiteUrl", :href="fm.websiteUrl", target="_blank") <strong>Website:</strong> {{fm.website}}
+    .main-div
+      h2 {{fm.title}}
+      p.seminar-info-list {{fm.startDate}} - {{fm.endDate}} 
+      p(v-if="descriptionShort",  v-html='descriptionShort')
+      p(v-if="description",       v-html='description')
+      p(v-if="fm.honorar")      <strong>Honorar:</strong>                     {{fm.honorar}}
+      p(v-if="fm.kursgebuehr")  <strong>Kursbebühr:</strong>                  {{fm.kursgebuehr}}
+      p(v-if="fm.unterkunft")   <strong>Unterkunft und Verpflegung:</strong>  {{fm.unterkunft}}
+      p(v-if="fm.performers")   <strong>Leitung:</strong>                     {{fm.performers}}
+      p(v-if="dozentenbeschreibung", v-html="dozentenbeschreibung")
+      //a(v-if="fm.websiteUrl", :href="fm.websiteUrl", target="_blank") <strong>Website:</strong> {{fm.website}}
 
 </template>
 
